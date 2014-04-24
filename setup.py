db00f44642cebe0247ec54921274d0488cc40001
Chris Moffitt <chris@moffitts.net>
2014-04-24 18:59:17 -0500

    Bumping rev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-caching-app-plugins',
-    version='0.1.3',
+    version='0.1.4',
     description='',
     author='Bruce Kroeze',
     author_email='bruce@ecomsmith.com',
