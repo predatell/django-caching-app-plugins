1df122cf2d856c4ae6a2853b6621f440050b1c13
predatell <predatell@disroot.org>
2022-09-01 16:50:11 +0300

    gettext instead ugettext

--- app_plugins/forms.py
+++ app_plugins/forms.py
@@ -1,12 +1,12 @@
 
 from django import forms
-from django.utils.translation import ugettext, ugettext_lazy as _
+from django.utils.translation import gettext, gettext_lazy as _
 
 from app_plugins.models import PluginPoint, Plugin, LABEL_RE
 
 def validate_label(value):
     if not LABEL_RE.search(value):
-        raise forms.ValidationError(ugettext(
+        raise forms.ValidationError(gettext(
             u"This value must contain only letters, "
             u"numbers, underscores, and '.' dots."
         ))
